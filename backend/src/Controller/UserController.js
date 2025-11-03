2683d7c9307c0fe94a3fa65b666557383a00290d
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-11-03 18:06:30 +0100

    sign up details

--- backend/src/Controller/UserController.js
+++ backend/src/Controller/UserController.js
@@ -4,10 +4,31 @@ const userModel = require("../models/UserModel")
 
 const userSignUp = async(req, res)=>{
     console.log(req.body)
-
     try {
         const { username, email, password} = req.body
         console.log(username)
+
+        if(!username || !email || !password){
+            console.log("missing detail")
+        }else{
+            const user = await  userModel.create({
+                username,
+                email,
+                password,
+            })
+
+       
+            if(user){
+                const userData = {
+                    id: user._id,
+                    username: user.username,
+                    email: user.email,
+                  };
+    
+                console.log(user)
+                return res.status(200).send({message:"sign up successful", status:true, user:userData})
+            }
+        }
         
     } catch (error) {
         console.log(error)
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const connect = require("./Dbconfig/dbconfig.js")
-
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
+const userRouter = require("./routes/auth")
+
 const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use("/user", userRouter)
+
 
 app.get("/", (req, res) => res.send("Chat backend running"));
 
