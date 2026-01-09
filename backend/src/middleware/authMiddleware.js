e5e9c6483cb0af8affea08b7e28099d29ebce69f
ogunnaike1 <ogunnaikeusman17@gmail.com>
2026-01-09 23:41:34 +0100

    commit

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Not authorized" });
+
   }
 
   const token = authHeader.split(" ")[1];
